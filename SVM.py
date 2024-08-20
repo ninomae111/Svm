7c32ed6ab7ee6034ef1b1051a89051857570c8e5
ninomae111 <liu.jianying@zsxmhospital.com>
2024-08-20 17:10:56 +0800

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -47,8 +47,15 @@ education_options = {
 }
 
 # Define feature names
-feature_names = [ccb, last_bowel_movement_was_clear_liquid, split_dose,
-                  in_hospital_bowel_preparation, bowel_movement_status, activity_level, education]
+feature_names = [
+    'CCB', 
+    'Last bowel movement was clear liquid', 
+    'Split dose',
+    'In hospital bowel preparation', 
+    'Bowel movement status', 
+    'Activity level', 
+    'Education'
+]
 
 # Streamlit user interface
 st.title("Bowel Preparation Predictor")
@@ -75,8 +82,15 @@ activity_level = st.selectbox("Activity level:", options=list(activity_level_opt
 education = st.selectbox("Education:", options=list(education_options.keys()), format_func=lambda x: education_options[x])
 
 # Process inputs and make predictions
-feature_values = [ccb, last_bowel_movement_was_clear_liquid, split_dose,
-                  in_hospital_bowel_preparation, bowel_movement_status, activity_level, education]
+feature_values = [
+    ccb, 
+    last_bowel_movement_was_clear_liquid, 
+    split_dose,
+    in_hospital_bowel_preparation, 
+    bowel_movement_status, 
+    activity_level, 
+    education
+]
 
 # Convert features to DataFrame to match the format used during training
 features = pd.DataFrame([feature_values], columns=feature_names)
