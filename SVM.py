3648a0e0e486583a87e0016a8cc4862a4788525e
ninomae111 <liu.jianying@zsxmhospital.com>
2024-08-21 00:21:54 +0800

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -120,7 +120,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 model_to_explain = model
 
 # 假设 custom_data 是一个包含具体参数的数据框
-    custom_data = params
+    custom_data = params
     print("custom_data is:")
     print(custom_data)
     print("normal_data is:")
